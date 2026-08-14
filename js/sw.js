8b0bcab790aa5abf87c1b0ac72abf11c4d0fde8f
Pedro Martins <pedromartinsss996@gmail.com>
2026-08-14 20:08:12 -0300

    Implementação dos eventos no service worker

--- js/sw.js
+++ js/sw.js
@@ -7,3 +7,23 @@ var filesTocache = [
     '/css/style.css',
     './js/main.js'
 ]
+
+self.addEventListener('install', event => {
+    event.waitUntil(
+        caches.open(cacheName)
+            .then(cache =>
+                cache.addAll(filesTocache)
+        )
+    );
+});
+
+self.addEventListener( 'fetch', event =>{
+    event.respondWith(
+        caches.match(event.request)
+        .then( response => {
+            return response ||
+               fetch(event.request)
+
+        })
+    );
+});
